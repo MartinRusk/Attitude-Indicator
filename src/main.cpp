bf9fc1b3b325bc1847eaeefab1f6faa289348fa2
MartinRusk <martin@ruskowski.de>
2022-12-04 21:50:15 +0100

    Format cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ Stepper roll(1, 0, 2, 3);
 Stepper pitch(4, 5, 6, 7);
 #define cal_roll 300
 #define max_roll 270
-#define scale_roll 6  // steps/°
+#define scale_roll 6 // steps/°
 #define cal_pitch 48
 #define max_pitch 36
 #define scale_pitch 2 // steps/°
@@ -15,11 +15,11 @@ Stepper pitch(4, 5, 6, 7);
 float roll_angle;
 float pitch_angle;
 
-void setup() 
+void setup()
 {
   // initialize the serial port
   Serial.begin(XPLDIRECT_BAUDRATE);
-  xp.begin("AttitudeIndicator");  
+  xp.begin("AttitudeIndicator");
   xp.registerDataRef("sim/cockpit2/gauges/indicators/roll_electric_deg_pilot", XPL_READ, 50, 0.2, &roll_angle);
   xp.registerDataRef("sim/cockpit2/gauges/indicators/pitch_electric_deg_pilot", XPL_READ, 50, 0.5, &pitch_angle);
 
@@ -54,14 +54,14 @@ void set_pitch(float angle)
 void test()
 {
   delay(1000);
-  for (int i=-60; i<=60; i+=1)
+  for (int i = -60; i <= 60; i += 1)
   {
     set_roll((float)i);
     set_pitch((float)i * 0.5);
     wait();
     delay(10);
   }
-  for (int i=60; i>=-60; i-=1)
+  for (int i = 60; i >= -60; i -= 1)
   {
     set_roll((float)i);
     set_pitch((float)i * 0.5);
@@ -70,7 +70,7 @@ void test()
   }
 }
 
-void loop() 
+void loop()
 {
   xp.xloop();
   set_roll(roll_angle);
